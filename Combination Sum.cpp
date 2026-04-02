eca9ffeae2832de2a52d0d05ebf762064bebc9ec
Minh Anh <minhanh15104@gmail.com>
2026-04-02 14:28:59 +0700

    Initial commit: Add solution files

---
+++ Combination Sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    void backtrack(vector<int>& candidates, int target, int start,
+                   vector<int>& path, vector<vector<int>>& result) {
+        
+        if (target == 0) {
+            result.push_back(path);
+            return;
+        }
+        
+        if (target < 0) return;
+        
+        for (int i = start; i < candidates.size(); i++) {
+            
+            path.push_back(candidates[i]);
+            
+            backtrack(candidates, target - candidates[i], i, path, result);
+            
+            path.pop_back();
+        }
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        
+        vector<vector<int>> result;
+        vector<int> path;
+        
+        backtrack(candidates, target, 0, path, result);
+        
+        return result;
+    }
+};
