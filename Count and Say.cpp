688dad9e4cf35d6def5d527e5adbc4750df37bb8
Minh Anh <minhanh15104@gmail.com>
2026-04-02 14:28:03 +0700

    Initial commit: Add solution files

---
+++ Count and Say.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        
+        string result = "1";
+        
+        for(int i = 2; i <= n; i++){
+            
+            string temp = "";
+            
+            for(int j = 0; j < result.size(); ){
+                
+                char current = result[j];
+                int count = 0;
+                
+                while(j < result.size() && result[j] == current){
+                    count++;
+                    j++;
+                }
+                
+                temp += to_string(count) + current;
+            }
+            
+            result = temp;
+        }
+        
+        return result;
+    }
+};
