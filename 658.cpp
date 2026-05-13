43fc26a1f39c827369e6c0d52eab5eecc71f8c85
Minh Anh <minhanh15104@gmail.com>
2026-05-13 14:37:20 +0700

    658.cpp

---
+++ 658.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr,
+                                    int k,
+                                    int x) {
+
+        int left = 0;
+        int right = arr.size() - k;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            // bên phải gần x hơn
+            if (x - arr[mid] >
+                arr[mid + k] - x) {
+
+                left = mid + 1;
+            }
+
+            // bên trái tốt hơn
+            else {
+
+                right = mid;
+            }
+        }
+
+        return vector<int>(arr.begin() + left,
+                           arr.begin() + left + k);
+    }
+};
